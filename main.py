51c44df680654f9f80213ed7183e1c6fc87dbb31
WarlordDK <krsdiman@gmail.com>
2025-10-05 17:10:51 +0500

    corsMiddleware

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from endpoints.secrets import secret_router
 from endpoints.users import user_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router, prefix='/users')
 app.include_router(secret_router, prefix='/secrets', tags=["openbao"])
 
